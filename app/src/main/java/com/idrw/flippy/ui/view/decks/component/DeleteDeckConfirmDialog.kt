986c28ce9794db487139e6bbc15208b7a2aee6a3
idontreallywolf <nedim.kanat.lp@gmail.com>
2024-12-15 04:41:20 +0100

    modify deleteConfirmation dialog colors

--- app/src/main/java/com/idrw/flippy/ui/view/decks/component/DeleteDeckConfirmDialog.kt
+++ app/src/main/java/com/idrw/flippy/ui/view/decks/component/DeleteDeckConfirmDialog.kt
@@ -1,9 +1,11 @@
 package com.idrw.flippy.ui.view.decks.component
 
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 
@@ -19,15 +21,23 @@ fun DeleteDeckConfirmDialog(
     if (!isVisible) return
 
     AlertDialog(
-        icon = { Icon(icon, contentDescription = "Example Icon") },
+        icon = { Icon(icon, contentDescription = "Warning Icon") },
         title = { Text(text = dialogTitle) },
         text = { Text(text = dialogText) },
         onDismissRequest = { onDismiss() },
         confirmButton = {
-            TextButton(onClick = { onConfirm() }) { Text("Confirm") }
+            Button(onClick = { onConfirm() }, colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )) { Text("Confirm") }
         },
         dismissButton = {
-            TextButton(onClick = { onDismiss() }) { Text("Dismiss") }
-        }
+            Button(onClick = { onDismiss() }, colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondary,
+                contentColor = MaterialTheme.colorScheme.onSecondary
+            )) { Text("Dismiss") }
+        },
+        containerColor = MaterialTheme.colorScheme.background,
+        textContentColor = MaterialTheme.colorScheme.onBackground,
     )
 }
